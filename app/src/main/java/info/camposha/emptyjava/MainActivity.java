1c278d696335c40d39eedbb756546092fab1b3fc
Oclemy <oclemmi@gmail.com>
2021-03-10 11:43:26 +0300

    Initial commit

---
+++ app/src/main/java/info/camposha/emptyjava/MainActivity.java
@@ -0,0 +1,14 @@
+package info.camposha.emptyjava;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
